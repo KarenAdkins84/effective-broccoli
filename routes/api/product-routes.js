3eab28615d2196e78c59e1fbc436812ce766359d
Nerakma84 <fnk8476@gmail.com>
2022-11-01 23:08:47 -0500

    GET routes working on insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -10,7 +10,6 @@ router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
       include: [{ model: Category },
-        { model: ProductTag},
       { model: Tag }]
     });
     res.status(200).json(productData);
@@ -24,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`FindOneByID
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findOne(req.params.id, {
+    const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category },
       { model: ProductTag},
     { model: Tag }]
